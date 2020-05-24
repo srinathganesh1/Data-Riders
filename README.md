61914317eeaa5e6c69316f246f23fab6f604deee
Srinath Ganesh <sganesh@galaxiasol.com>
2020-05-24 13:15:39 +0530

    Readme

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Data-Riders
 
+Understand Data
+- [ ] not sure
+
 Feature Checklist
 - [ ] Missing Values
-- [ ] Visualize Interpretations
+- [ ] Data Manipulation
 - [ ] Cluster Analysis
 - [ ] Word Cloud
+- [ ] Visualize Interpretations
 
 Presentation Checklist
 - [ ] PPT 10 slides (1+8+1)
--- src/main.R
+++ src/main.R
@@ -1,1 +1,35 @@
-print("Hello World!")
+# Each team member would need to set their directory path to variable "project_dir"
+# we will make sure everything else is relative to "project_dir"
+project_dir <- "your_directory_path/Data-Riders"
+setwd(project_dir)
+
+# Get the Raw Data from the file
+read_file <- function () {
+  data <- read.csv("data/cereals_data.csv")
+  return (data)
+}
+
+# Handle missing values
+handle_missing_values <- function (input) {
+  output <- input
+  # any handling of missing values
+  return (output)
+}
+
+# Clearn the data to make it good for processing
+# eg. delete unwanted rows, change data types etc
+data_processing <- function (input) {
+  output <- input
+  output <- handle_missing_values(output)
+  # some data processing steps that would be required
+  return (output)
+}
+
+# Get the Data Frame for the data
+get_data <- function () {
+  raw <- read_file()
+  data <- data_processing(raw)
+}
+
+data <- get_data()
+View(data)
