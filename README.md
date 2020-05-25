4f3d32e761f8b9afdea4d9c3cda8143ccd19b766
Srinath Ganesh <sganesh@galaxiasol.com>
2020-05-25 16:39:57 +0530

    Readme

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ Presentation Checklist
 - [ ] Markdowns
 - [ ] Website
 - [ ] Find a good article from internet related to calories, nutrients and ratings and plot a catchy wordcloud
+
+
+Possible Case Studies
+- list of bad parameters (what can bring the product down)
+- list of good parameters (what can increase sales -- rating for this dataset)
+- if u want to improve rating by X, whats the best way
+- if u want to improve rating by p%, whats the best way 
--- src/main.R
+++ src/main.R
@@ -1,7 +1,8 @@
 # Each team member would need to set their directory path to variable "project_dir"
 # we will make sure everything else is relative to "project_dir"
-project_dir <- "your_directory_path/Data-Riders"
+project_dir <- "/home/admin-12/Documents/IMARTICUS/Data-Riders"
 setwd(project_dir)
+library(dplyr)
 
 # Get the Raw Data from the file
 read_file <- function () {
@@ -13,6 +14,7 @@ read_file <- function () {
 handle_missing_values <- function (input) {
   output <- input
   # any handling of missing values
+  output <- na.omit(output)
   return (output)
 }
 
@@ -33,3 +35,52 @@ get_data <- function () {
 
 data <- get_data()
 View(data)
+
+summary(data)
+
+
+names(data)
+#boxplot(data$calories)
+#boxplot(data$protein)
+#boxplot(data$fat)
+#boxplot(data$sodium)
+#boxplot(data$fiber)
+#boxplot(data$carbo)
+#boxplot(data$sugars)
+#boxplot(data$potass)
+#boxplot(data$vitamins)
+#boxplot(data$shelf)
+#boxplot(data$weight)
+#boxplot(data$rating)
+
+#plot(select(data, c("calories", "protein", "fat", "sodium", "rating")))
+
+#cor.test(data$weight, data$rating)
+#cor.test(data$calories, data$rating)
+#data[-c(1, 2, 3)]
+correlation <- cor(data[-c(1, 2, 3)])
+correlation
+correlation
+
+#cor.test(data$calories, data$carbo)
+
+cor.test(data$rating, data$sugars)
+pairs(formula=~rating+sugars, data=data)
+
+#dev.off()
+
+# JUST BETWEEN SUGAR AND RATING!!!!!
+# so dont expect it to be correct!
+model <- lm(rating~sugars+calories+protein+fat+sodium+fiber+carbo+potass+vitamins+shelf+weight+cups, data=data)
+model
+
+data[data$name=='Corn_Flakes', ]
+
+a <- data[1,]
+a
+b <- a
+b["fat"] <- 20
+
+#data.frame(data$sugars, data$rating)
+predict(model, b, interval="prediction")
+data.frame(data$rating, data$sugars, predict(model, data.frame(sugars=data$sugars), interval="prediction"))
